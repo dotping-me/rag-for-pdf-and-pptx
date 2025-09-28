3f5d5ef629b934f4686b379b1bb14e2537451370
dotping-me <dotping.hpg@gmail.com>
2025-09-28 17:20:35 +0400

    Tweaked Slide # display in Output MD

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # NOTE: This is just an MVP
 
-from typing import Any
+from typing import Any, List
 from markitdown import MarkItDown
+
+import re
 
 def main() -> bool:
     """
@@ -22,6 +24,14 @@ def main() -> bool:
         print("Converting PDF to MD")
         
         mdText = mdConverter.convert(r"{}".format(filepath)).text_content
+
+        # HACKJOB: "<!-- Slide 1 -->" is set by default
+        lines: List[str] = mdText.split("\n")
+        for i, ln in enumerate(lines):
+            if "<!-- Slide number:" in ln:
+                lines[i] = "Slide " + ln[18: ln.index(">") - 2] + "\n---\n"
+
+        mdText = "\n".join(i for i in lines)
         with open("test.md", "w") as f:
             f.write(mdText)
 
