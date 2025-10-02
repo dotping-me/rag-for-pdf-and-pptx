f5bb204aea752feb7ededa9f6eeb6e65fe90b975
dotping-me <dotping.hpg@gmail.com>
2025-10-02 11:06:27 +0400

    Added file transfer to backend (Not much)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Any, Dict, Optional
 import webview, json, os
 
 class App:
@@ -9,6 +9,20 @@ class App:
     # API Layer
     def api_hello_world(self, username: str) -> str:
         return json.dumps({ "message": f"Hello {username}!" })
+
+    def api_rag_service(self, json_obj: Dict[str, Any]) -> str:
+        """
+        Calls RAG service from backend
+
+        Steps:
+            1. Takes file input from user input (app)
+            2. Stores file in backend buffer, I guess?
+            3. Calls RAG service using CLI tooling
+            4. Catches and returns JSON response
+        """
+
+        print(json_obj)
+        return "" 
 
     def home(self):
         """ Serves index.html """
@@ -22,11 +36,12 @@ class App:
 
     def run(self):
         """ Runs App, duh... """
+        
         self.home()
 
         # Exposes API layer
         if self.window:
-            self.window.expose(self.api_hello_world)
+            self.window.expose(self.api_rag_service)
             webview.start(debug = True)
 
 if __name__ == "__main__":
